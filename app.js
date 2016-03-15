66c8da1e90d38e8b5719c156c29fb9b3c01c91e5
Gman24 <stuckinabox@live.com>
2016-03-15 05:56:49 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 $.jqMVC
 .data({
 	app_path  : 'http://r3wt.github.io/jqMVC/',//app requires to know its base_path. 
-	view_path : 'views/', //view path
-	module_path : 'modules/', //where to look for modules.
-	element : $('#main'), //main view element where views are rendered.
-	debug : true ,//whether to log debug info the console. $.jqMVC.debug() will work regardless of this flag.
-	binding_override: true//allow to override default bindings of the framework. you can see these in src/events.js they are properties of the `evt` object.
+	view_path : 'views/',
+	module_path : 'modules/',
+	element : $('#main'),
+	debug : false,
+	binding_override: false
 })
 .before() //start the loading indicator
 .setView({
-	//use twig to load templates and render views
 	render: function(file,args,callback,el){
 		try{
 			if(typeof args !== 'undefined' && typeof default_args !== 'undefined'){
 				args = $.extend(default_args,args);
+			}else{
+				args = default_args;
 			}
-			$.get(app_path+view_path+file,function(htmlData){
+			$.get(location.origin+'/'+view_path+file,function(htmlData){
 				if(htmlData.indexOf('<!doctype') > 0){
 					throw 'File not Found';
 				}
@@ -23,6 +24,8 @@ $.jqMVC
 				target.html(twig({data:htmlData}).render(args));
 				if (typeof callback === "function") {
 					callback.call();
+				}else{
+					$.jqMVC.done();//if no callback is defined call done
 				}
 			}).fail(function(){
 				throw 'Request Failed';
@@ -37,8 +40,10 @@ $.jqMVC
 		try{
 			if(typeof args !== 'undefined' && typeof default_args !== 'undefined'){
 				args = $.extend(default_args,args);
+			}else{
+				args = default_args;
 			}
-			$.get(app_path+view_path+file,function(htmlData){
+			$.get(location.origin+'/'+view_path+file,function(htmlData){
 				if(htmlData.indexOf('<!doctype') > 0){
 					throw 'File not Found';
 				}
@@ -55,42 +60,6 @@ $.jqMVC
 			$.jqMVC.trigger('viewError').done();
 		}
 	}
-})
-.setNotification({
-	alert: function(_m,_t){
-		console.log(_m);
-		return noty({
-			text: _m,
-			type:_t,
-			layout:'topCenter',
-			timeout: 10000,
-			animation: {
-				open: {height: 'toggle',opacity:'1.0'}, // jQuery animate function property object
-				close: {height: 'toggle',opacity:'0'}, // jQuery animate function property object
-				easing: 'swing', // easing
-				speed: 500 // opening & closing animation speed
-			}
-		});
-	},
-	confirm: function(_text,callback,args){
-		var args = args || [];
-		noty({
-			text: _text,
-			layout: 'center',
-			modal: true,
-			buttons: [
-				{addClass: 'button tiny primary', text: 'Continue', onClick: function($noty) {
-						$noty.close();
-						callback.apply(window,args);
-					}
-				},
-				{addClass: 'button tiny alert', text: 'Cancel', onClick: function($noty) {
-						$noty.close();
-					}
-				}
-			]
-		});
-	},
 })
 //create a middleware to load our modules.
 .loadModules(['controllers.js','bindings.js','services.js','routes.js'])
