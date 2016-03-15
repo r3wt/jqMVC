d912ce6c463d13cf6ea88a72c6c3349d57314131
Gman24 <stuckinabox@live.com>
2016-03-15 05:25:49 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 $.jqMVC
 .data({
-	app_path  : 'http://jqmvc.openex.info/seed/',//app requires to know its base_path. 
+	app_path  : 'http://r3wt.github.io/jqMVC/',//app requires to know its base_path. 
 	view_path : 'views/', //view path
 	module_path : 'modules/', //where to look for modules.
 	element : $('#main'), //main view element where views are rendered.
 	debug : true ,//whether to log debug info the console. $.jqMVC.debug() will work regardless of this flag.
 	binding_override: true//allow to override default bindings of the framework. you can see these in src/events.js they are properties of the `evt` object.
-})
-.setProgress({
-	//use nprogress for loading indicators.
-	start: NProgress.start,
-	stop: NProgress.done
 })
 .before() //start the loading indicator
 .setView({
@@ -99,51 +94,6 @@ $.jqMVC
 })
 //create a middleware to load our modules.
 .loadModules(['controllers.js','bindings.js','services.js','routes.js'])
-.add(function(stack){
-	/*
-	//this example shows you how to use a middleware to initialize your app on first load. 
-	//the example below adds a csrf token to all ajax requests and calls a service to set some global variables.
-	$.getJSON(api_path + 'initialize_device',function(data){
-		$.jqMVC.data({csrf: data.response});
-		//this example is really useful. for a file upload, you need to append to the form data. other forms you just use params and extend.
-		$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-			if(originalOptions.url.indexOf('.twig') === -1){
-				if(originalOptions.data instanceof FormData){
-					options = originalOptions;
-					options.data.append('c',csrf);
-				}else{
-					options.data = $.param($.extend(originalOptions.data, { c: csrf }));
-				}
-			}
-		});
-		svc.getData(function(){
-			stack.next()
-		});
-	});
-	*/
-	stack.next();//when your middleware is finish, it should call stack.next()
-})
-.listen('firstRun',function(){
-	$(document).ajaxError(function(){
-		$.jqMVC.trigger('apiError');
-	});
-})
-.listen('apiError',function(){
-	/* 
-	//example how to listen for errors
-	$.jqMVC.render('error.twig',{error: 'Unknown Error'},function(){
-		$.jqMVC.done();
-	});
-	*/
-})
-.listen('notFound',function(){
-	/*
-	//example of how to listen for 404s
-	$.jqMVC.render('error.twig',{error: '404 Not Found'},function(){
-		$.jqMVC.done();
-	});
-	*/
-})
 .listen('before.go',function(){
 	//if you need to do some stuff before navigation, like start a progress bar, clear the view, scroll up, etc.
 	$.jqMVC.before();//before starts the progress bar
